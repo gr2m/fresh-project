928f4e1146ef3a5953e974ede63b2416c58bf8c2
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2025-03-31 20:37:03 -0700

    fix island

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,7 +8,7 @@ import * as $api_joke from "./routes/api/joke.ts";
 import * as $countdown from "./routes/countdown.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
-import * as $countdown_1 from "./islands/countdown.tsx";
+import * as $Countdown from "./islands/Countdown.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
@@ -21,7 +21,7 @@ const manifest = {
     "./routes/index.tsx": $index,
   },
   islands: {
-    "./islands/countdown.tsx": $countdown_1,
+    "./islands/Countdown.tsx": $Countdown,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
